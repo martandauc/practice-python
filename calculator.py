cb0e434f76fc693257d54a83ef66e1fc124be631
martanda <138761780+martandauc@users.noreply.github.com>
2025-04-16 11:15:37 +0530

    Create calculator.py
    Add calculator.py with add and subtract functions

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print("Simple Calculator")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("Addition:", add(x, y))
+print("Subtraction:", subtract(x, y))
